e957a6cc1866a07e734afe92c0954ef990706e03
EMG2311 <germanmontirubio@gmail.com>
2024-06-13 17:16:41 -0300

    Se carga configuracion inicial del proyecto

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment webHostEnvironment1)
+        {
+            if (webHostEnvironment1.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(Options=>Options.MapControllers());
+
+        }
+    }
+}
